f3d6bcc8ea4adc8fc9277b0a2f6e02cfaa6fbf3f
Karl Rohr <kjrohr@fullsail.edu>
2015-10-30 21:24:43 -0400

    Removed some lines
    Github added some lines?

--- e.sh
+++ e.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-<<<<<<< HEAD
 dir="$1"
 count=$(ls "$dir" | wc -l)
 echo File Count:  $count
-=======
->>>>>>> origin/gh-pages
 
 test=$(ls -l "$dir" | awk '{s += $5} END {print s/1024}')
 echo Total of all Files: $test
